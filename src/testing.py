7825468130223b5aebaa8e5eedd21e5b5c0f4405
LordBurtz <fridolin.karger@gmail.com>
2021-07-15 22:22:48 +0200

    integrated w/ qtdesigner stuff hard lol will play tmrw with qtdesigner more

--- src/testing.py
+++ src/testing.py
@@ -1,1 +1,28 @@
-print("hello world!")
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtGui import QIcon
+from demo import Ui_Dialog
+
+
+def start():
+    app = QApplication(sys.argv)
+    window = QWidget()
+    window.show()
+    window.setGeometry(100, 100, 500, 500)
+    window.setWindowTitle("lel")
+    window.setWindowIcon(QIcon("icons/lambda-icon.jpg"))
+
+    sys.exit(app.exec_())
+
+def testApp():
+    app = QtWidgets.QApplication(sys.argv)
+    Dialog = QtWidgets.QDialog()
+    ui = Ui_Dialog()
+    ui.setupUi(Dialog)
+    Dialog.show()
+    sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    testApp()
+    #start()
